380534743872b20e2a63bab5c03db5b843f1dd3e
youssraa <tayachii.yosra@gmail.com>
2025-04-26 23:37:38 +0200

    update express v4 /* to v5 /*splat

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -30,7 +30,7 @@ getUsers : async()=>{
 getMessages: async(userId)=>{
     set({isMessagesLoading : true });
     try {
-        const res = await axiosInstance.get(`/messages/all/${userId}`) 
+        const res = await axiosInstance.get(`/messages/${userId}`) 
         set({messages: res.data})  
     } catch (error) {
         toast.error(error.response.data.message);
