8f3ad1ece9ad2a6e6f848bc1fc0059cd421c0a5a
youssraa <tayachii.yosra@gmail.com>
2025-04-26 22:39:44 +0200

    developement to development

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios' ;
 
 
 export const axiosInstance = axios.create({
-    baseURL :import.meta.env.MOD_ENV === "developement" ? " http://localhost:5001/api" : "/api" ,
+    baseURL :import.meta.env.MOD_ENV === "development" ? " http://localhost:5001/api" : "/api" ,
     withCredentials : true 
     
 })
